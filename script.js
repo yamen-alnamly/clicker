10b96c83725477932382a5698732740f91501e7d
Yamen <125800880+yamen-alnamly@users.noreply.github.com>
2023-04-09 15:59:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let button = 1;
-
+let colors = [ '' , 'red' , 'yellow','green','purple', 'white'];
+let n=0
 function add() {
   document.querySelector('#ball').innerHTML = button++
-  console.log(button)
+  document.getElementById('clicker').style.backgroundColor = colors[n]
+ for(let m =1 ; m < colors.length ; m++)
+  {
+   if( document.querySelector('#ball').innerHTML == 100*m)
+    n++
+  }
 }
+
